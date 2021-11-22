728dcbd53364c5fc56985b2d646fb2a6c7ce4e12
FishSticks-stack <spicykiwi78@gmail.com>
2021-11-22 14:55:31 -0600

    Initial commit

---
+++ covid tweets.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# reading in the data tweets
+data = pd.read_csv("tweets_tagged.csv")
+# TODO should the string values be encoded into integers?
+
+
+
+# TODO split the data between the original 'train'/answers and the rest as 'testing' to test the trained model
+
+# splitting up the data into test and train
+
