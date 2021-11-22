a3fa2ef928967039873df1e8872a872501f05cf5
FishSticks-stack <spicykiwi78@gmail.com>
2021-11-22 15:15:58 -0600

    2nd commit

--- covid tweets.py
+++ covid tweets.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 # reading in the data tweets
 data = pd.read_csv("tweets_tagged.csv")
 # TODO should the string values be encoded into integers?
 
 
-
 # TODO split the data between the original 'train'/answers and the rest as 'testing' to test the trained model
+# GERMAN CREDIT FOR SPLITTING DATA
 
 # splitting up the data into test and train
 
