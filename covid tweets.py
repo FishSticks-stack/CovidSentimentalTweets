bc5cbc0cff758b79311f2ad712c0f1edde96acdf
vanessamartinez <spicykiwi78@gmail.com>
2021-11-23 17:29:30 -0600

    split the data into train and testing

--- covid tweets.py
+++ covid tweets.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn import tree
 
 # reading in the data tweets
-data = pd.read_csv("tweets_tagged.csv")
-# TODO should the string values be encoded into integers?
+data = pd.read_csv("tweets_tagged.csv", encoding="ISO-8859-1")
 
+# getting all attributes except the label column, 'sr no, tweet'
+attributes = [col for col in data.columns if col != "label"] # remove sr no column??
 
-# TODO split the data between the original 'train'/answers and the rest as 'testing' to test the trained model
-# GERMAN CREDIT FOR SPLITTING DATA
+# columns, testing data, label answers, label answers
+train_x, test_x, train_y, test_y = train_test_split(data[attributes], data["label"], test_size=0.3, random_state=123)
 
-# splitting up the data into test and train
+print("Label counts (1,2,3)")
+print(data["label"].value_counts())
 
